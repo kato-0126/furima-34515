4d041d72bf2cafa4ebba059b4a2d11fd5f00aea5
kato-0126 <kato.t.0126@gmail.com>
2021-03-04 15:00:38 +0900

    バリデーション＋ログイン・ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
     end
   end
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth, presence: true
+
+  with_options presence:true, format:{ with:  /\A[ぁ-んァ-ヶ一-龥々]+\z/, message:'全角文字を使用してください'} do
+    validates :first_name
+    validates :last_name
+  end
+ with_options presence: true,format:{ with:  /\A[ァ-ン]+\z/, message:'カタカナを使用してください' } do
+  validates :first_name_kana
+  validates :last_name_kana
+ end
+ validates :birth, presence: true, format:{with: /[\d]/}
 end

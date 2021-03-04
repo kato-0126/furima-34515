d12bd75200fedb4b8a31adaef4feab3d418e3dc6
kato-0126 <kato.t.0126@gmail.com>
2021-03-04 21:43:04 +0900

    Faker+Gimei実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name            {Gimei.first.kanji}
+    last_name             {Gimei.last.kanji}
+    first_name_kana       {Gimei.first.katakana}
+    last_name_kana        {Gimei.last.katakana}
+    birth                 {Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+  it 'nickname,email,password,first_name(kana),last_name(kana),birthが存在すれば登録できる' do
+    expect(@user).to be_valid
+  end
    it 'ニックネームが空では登録できない' do
-    
+
    end
    it 'emailが空では登録できない' do
    end 

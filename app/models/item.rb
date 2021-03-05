b0717142009924fd37b719f73672235032b994cb
kato-0126 <kato.t.0126@gmail.com>
2021-03-05 19:34:15 +0900

    ActiveHushの導入、データ入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :date
 end

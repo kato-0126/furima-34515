f093dedbce8b1e3de0c072d83375e07e68f952ed
kato-0126 <kato.t.0126@gmail.com>
2021-03-08 16:59:26 +0900

    updateアクション＋他ユーザー編集の制限を設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [ :index,:show ]
+  before_action :move_to_index, only: [ :edit ]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,20 +22,31 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
   def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
     if @item.save
-      redirect_to 
+      redirect_to item_path(@item.id)
     else
-      render :new      
+      render :edit
     end
   end
 
   private
 
-  def
-    item_params
+  def item_params
     params.require(:item).permit(:name, :category_id, :condition_id, :explain, :fee_id, :prefecture_id, :delivery_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    item = Item.find(params[:id])
+    unless item.user_id == current_user.id      
+      redirect_to root_path
+    end
   end
 end

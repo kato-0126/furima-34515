d1eefe5ecf570281499034c4b20a13d20db268da
kato-0126 <kato.t.0126@gmail.com>
2021-03-10 21:51:54 +0900

    Factory.Botの設定

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postal_code{'123-4567'}
+    prefecture_id{1}
+    city{Faker::Address.city}
+    address_line1{Faker::Address.street_address}
+    address_line2{Faker::Address.building_number}
+    phone_number{Faker::Number.number(digits: 11)}
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品購入機能実装' do
+    before do
+      user = FactoryBot.build(:user)
+      item = FactoryBot.build(:item)
+      order = FactoryBot.build(:order)
+      @order_address = FactoryBot.build(:order_address,user_id:user.id,item_id:item.id,order_id:order.id)
+    end
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@order_address).to be_valid
+    end
+  end
 end

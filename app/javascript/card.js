68af247f7d0debb4965da0caedad22b8c2052143
kato-0126 <kato.t.0126@gmail.com>
2021-03-11 11:58:18 +0900

    JavaScriptの設置／submitで発火まで

---
+++ app/javascript/card.js
@@ -0,0 +1,11 @@
+const pay = () =>{
+const form = document.getElementById("charge-form");
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  console.log("send-test")
+});
+
+}
+
+
+window.addEventListener("load",pay)
